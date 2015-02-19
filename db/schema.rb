f8b733fb30bea39aa1ef34b3269d500c3785dea0
isislvx <isislvx@yahoo.com>
2015-02-19 22:13:52 +0000

    added email to Ad model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150217192232) do
+ActiveRecord::Schema.define(version: 20150219221241) do
 
   create_table "ads", force: :cascade do |t|
     t.string   "name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20150217192232) do
     t.string   "img_url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "email"
   end
 
   create_table "users", force: :cascade do |t|
